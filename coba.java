685ca40c49f1566de78bcee1b71b9c5e9cbaf096
Zahra Meidira <zahrameidira2352003@gmail.com>
2022-12-22 06:42:55 +0700

    Create coba.java

---
+++ coba.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package satu;
+
+/**
+ *
+ * @author HP
+ */
+public class coba {
+    String name;
+    
+    String getName();
+    return name;
+    
+}
